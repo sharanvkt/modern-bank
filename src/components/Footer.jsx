74eef3883af0943e6c802d7aa09f8ea0934642f4
sharanvkt <sharanvkt@gmail.com>
2022-08-14 23:07:41 +0530

    none

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -36,8 +36,8 @@ const Footer = () => {
     </div>
 
     <div className='w-full flex justify-between items-center md:flex-row flex-col pt-6 border-t-[1px] border-t-[#3f3e45]'>
-      <p className='font-poppins font-normal text-center text-[18px] leading-[27px] text-white'>
-      Made With <span>❤️</span>  By <span src='https://github.com/sharanvkt' className='cursor-pointer'>Sharan</span>
+      <p src='https://github.com/sharanvkt' className='font-poppins font-normal text-center text-[18px] leading-[27px] text-white cursor-pointer'>
+      Made With <span>❤️</span>  By <span >Sharan</span>
       </p>
 
       <div className='flex flex-row md:mt-0 mt-6'>
